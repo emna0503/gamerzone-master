c4ca61694591ad45af4b031a4361a3ad40e44bfe
emna0503 <emnamallek9@gmail.com>
2024-05-12 23:21:47 +0100

    edit saved

--- bakend/users/urls.py
+++ bakend/users/urls.py
@@ -3,7 +3,7 @@ from .views import CreateUserAPIView,CreateFollowAPIView, FindUser,suggested_fri
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
-
+#test
 urlpatterns = [
     path('auth/', (include('rest_framework.urls', namespace='rest_framework'))),
     path('token',obtain_auth_token),
